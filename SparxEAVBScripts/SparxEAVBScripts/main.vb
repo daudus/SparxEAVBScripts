22dfa663230e29673e9adc8f52398fedd7915b7b
David <daudus@gmail.com>
2017-04-05 00:53:11 +0200

    Init

---
+++ SparxEAVBScripts/SparxEAVBScripts/main.vb
@@ -0,0 +1,21 @@
+﻿Namespace VBScript
+
+    Module Main
+        Sub Main()
+
+        End Sub
+
+        Sub Script()
+            !INC Wrappers.Include
+            'get the folder from the user
+            Dim folder
+            folder = New FileSystemFolder
+            folder = folder.getUserSelectedFolder("")
+            'show messagebox with the name of each subfolder
+            Dim subfolders, subfolder
+            For Each subfolder In folder.SubFolders
+                MsgBox("subfolder name: " & subfolder.Name)
+            Next
+        End Sub
+    End Module
+End Namespace
